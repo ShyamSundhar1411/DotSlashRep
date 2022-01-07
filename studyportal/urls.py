74e18eba54a78116e3c6d63e381ad5d6d36d725b
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-01-07 23:05:52 +0530

    Initial Stage

--- studyportal/urls.py
+++ studyportal/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 urlpatterns+=static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
-urlpatterns+=static(settings.MEDIA_URL,document.settings.MEDIA_ROOT)
+urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
